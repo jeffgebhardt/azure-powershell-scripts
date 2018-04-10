ed7486d8c8bb3be5187eb540faf20d8c4fb3e72f
Jeffrey Gebhardt <jeff@gebbey.com>
2018-04-10 11:28:18 -0700

    added delete vault script

---
+++ deleteVault.ps1
@@ -0,0 +1,12 @@
+﻿$vault = Get-AzureRmRecoveryServicesVault -Name <vault name goes here>
+Set-AzureRmRecoveryServicesVaultContext -Vault $vault
+
+$containers = Get-AzureRmRecoveryServicesBackupContainer -ContainerType AzureSQL -FriendlyName $vault.Name
+ForEach ($container in $containers) {
+    $items = Get-AzureRmRecoveryServicesBackupItem -container $container -WorkloadType AzureSQLDatabase
+    ForEach ($item in $items) {
+        Disable-AzureRmRecoveryServicesBackupProtection -item $item -RemoveRecoveryPoints -ea SilentlyContinue
+    }
+    Unregister-AzureRmRecoveryServicesBackupContainer -Container $container
+}
+Remove-AzureRmRecoveryServicesVault -Vault $vault -Verbose
